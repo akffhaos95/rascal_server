88fc736b1c98d56c95a8415dccffd8352d07ec73
origin <akffhaos95@naver.com>
2021-10-08 13:59:18 +0900

    register

--- src/main/java/com/example/rascalserver/Controller/AuthController.java
+++ src/main/java/com/example/rascalserver/Controller/AuthController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
+@Slf4j
 @Api(tags = {"1. Auth"})
 @RestController
 @RequiredArgsConstructor
@@ -44,9 +46,11 @@ public class AuthController {
     public CommonResult register(@ApiParam(value = "Email", required = true) @RequestParam("email") String email,
                                  @ApiParam(value = "Password", required = true) @RequestParam("password") String password,
                                  @ApiParam(value = "Name", required = true) @RequestParam("name") String name) {
+        log.info("Register : {} {} {}", email, password, name);
         accountJpaRepo.save(Account.builder()
                 .email(email)
                 .password(passwordEncoder.encode(password))
+                .name(name)
                 .roles(Collections.singletonList("ROLE_USER"))
                 .build());
         return responseService.getSuccessResult();
